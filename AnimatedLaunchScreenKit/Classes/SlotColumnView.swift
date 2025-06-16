0237e3a868504dfb2e173405f30ed659e55c120f
Brett Owers <owersbrett@gmail.com>
2025-06-15 22:43:21 -0600

    update

--- AnimatedLaunchScreenKit/Classes/SlotColumnView.swift
+++ AnimatedLaunchScreenKit/Classes/SlotColumnView.swift
@@ -32,10 +32,6 @@ public class SlotColumnView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    deinit {
-        stopScrolling()
-    }
 
     private func setupScrollView() {
         scrollView = UIScrollView()
